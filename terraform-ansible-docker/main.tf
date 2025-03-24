a6abb345036b50b121f2948ae5b63b86359bc449
harshal-003 <harshaljoshi9922@gmail.com>
2025-03-24 15:15:45 +0530

    add new files

--- terraform-ansible-docker/main.tf
+++ terraform-ansible-docker/main.tf
@@ -4,12 +4,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
   }
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
   region = "ap-south-1"
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
 
 resource "aws_instance" "app_server" {
@@ -74,6 +82,16 @@ resource "aws_security_group" "TF_SG" {
   }
 }
 
+resource "cloudflare_record" "portfolio_subdomain" {
+  zone_id = var.cloudflare_zone_id
+  name    = "harshal"
+  value   = aws_instance.app_server.public_ip
+  type    = "A"
+  ttl     = 1 
+  proxied = true
+}
+
+
 resource "local_file" "inventory" {
   filename = "ansible/inventory.ini"
   content  = <<-EOT
@@ -84,4 +102,8 @@ resource "local_file" "inventory" {
 
 output "instance_public_ip" {
   value = aws_instance.app_server.public_ip
+}
+
+output "portfolio_url" {
+  value = "http://harshal.purvesh.cloud"
 }
